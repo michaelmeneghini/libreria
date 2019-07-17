12b06f2bb41790fa53e181ed8fadc1dd9219988c
andres <ada.wong.gv@gmail.com>
2019-07-17 16:05:44 +0200

    Added Register Frame

--- src/Controller/ControllerLogin.java
+++ src/Controller/ControllerLogin.java
@@ -1,4 +1,12 @@
 package Controller;
 
+import javafx.fxml.FXML;
+
 public class ControllerLogin {
+
+        @FXML
+        public void loginButtonClicked(){
+            System.out.println("TEST");
+
+        }
 }

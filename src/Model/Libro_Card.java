848808b326045b3d093b375710f6f54e90ec49a7
Giovanni Scala <gioscala13@gmail.com>
2019-07-17 14:17:25 +0200

    Creation Libro, Libro_Card, Ordine, Responsabile; Abstract Utente, UtenteRegistrato

--- src/Model/Libro_Card.java
+++ src/Model/Libro_Card.java
@@ -1,5 +1,39 @@
 package Model;
 
-public class Libro_Card {
+import java.sql.Time;
 
+public class Libro_Card {
+    int  id;
+    Time data_emissione;
+    int punti;
+
+    public Libro_Card(int id, Time data_emissione, int punti) {
+        this.id = id;
+        this.data_emissione = data_emissione;
+        this.punti = punti;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Time getData_emissione() {
+        return data_emissione;
+    }
+
+    public int getPunti() {
+        return punti;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setData_emissione(Time data_emissione) {
+        this.data_emissione = data_emissione;
+    }
+
+    public void setPunti(int punti) {
+        this.punti = punti;
+    }
 }
--- src/Model/Ordine.java
+++ src/Model/Ordine.java
@@ -11,4 +11,97 @@ public class Ordine {
     String città;
     String nr_nome;
     String nr_cognome;
+
+    public Ordine(int id, String email, int prezzo, String pagamento, int punti, String indirizzo, int cap, String città, String nr_nome, String nr_cognome) {
+        this.id = id;
+        this.email = email;
+        this.prezzo = prezzo;
+        this.pagamento = pagamento;
+        this.punti = punti;
+        this.indirizzo = indirizzo;
+        this.cap = cap;
+        this.città = città;
+        this.nr_nome = nr_nome;
+        this.nr_cognome = nr_cognome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getPrezzo() {
+        return prezzo;
+    }
+
+    public String getPagamento() {
+        return pagamento;
+    }
+
+    public int getPunti() {
+        return punti;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public int getCap() {
+        return cap;
+    }
+
+    public String getCittà() {
+        return città;
+    }
+
+    public String getNr_nome() {
+        return nr_nome;
+    }
+
+    public String getNr_cognome() {
+        return nr_cognome;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPrezzo(int prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public void setPagamento(String pagamento) {
+        this.pagamento = pagamento;
+    }
+
+    public void setPunti(int punti) {
+        this.punti = punti;
+    }
+
+    public void setIndirizzo(String indirizzo) {
+        this.indirizzo = indirizzo;
+    }
+
+    public void setCap(int cap) {
+        this.cap = cap;
+    }
+
+    public void setCittà(String città) {
+        this.città = città;
+    }
+
+    public void setNr_nome(String nr_nome) {
+        this.nr_nome = nr_nome;
+    }
+
+    public void setNr_cognome(String nr_cognome) {
+        this.nr_cognome = nr_cognome;
+    }
 }

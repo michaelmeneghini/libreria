1d00dc510c4c360eb2ec7c2f7c8f5d5a5ece14c7
andres <ada.wong.gv@gmail.com>
2019-07-22 14:10:19 +0200

    added ControllerProfilo class and modified Profilo.fmxl and UtenteRegistrato.fxml

---
+++ src/Controller/ControllerProfilo.java
@@ -0,0 +1,41 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class ControllerProfilo {
+
+    @FXML
+    private AnchorPane anchor_pane;
+
+    @FXML
+    private Label label_nome;
+
+    @FXML
+    private Label lable_cognome;
+
+    @FXML
+    private Label lable_cap;
+
+    @FXML
+    private Label lable_email;
+
+    @FXML
+    private Label lable_città;
+
+    @FXML
+    private Label lable_cellulare;
+
+    @FXML
+    private Label lable_libroCard;
+
+    @FXML
+    public void modificaDatiButtonClicked(ActionEvent event){
+
+
+    }
+
+
+}
